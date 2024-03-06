2ee33c1aaefbbb250eef6dfd613a16e0ceb7b7bf
cuongnv1 <cuongnv1@mirabo-global.com>
2024-03-06 11:24:38 +0700

    feat: import gamecard

---
+++ Assets/_Data/BaseMonoBehaviour.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class BaseMonoBehaviour : MonoBehaviour
+{
+    protected virtual void Awake()
+    {
+        this.LoadComponents();
+    }
+
+    protected virtual void Start()
+    {
+        //For override
+    }
+
+    protected virtual void Reset()
+    {
+        this.LoadComponents();
+        this.ResetValue();
+    }
+
+    protected virtual void Update()
+    {
+        //For override
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        //For override
+    }
+
+    protected virtual void OnEnable()
+    {
+        //For override
+    }
+
+    protected virtual void OnDisable()
+    {
+        //For override
+    }
+
+    protected virtual void LoadComponents()
+    {
+        //For override   
+    }
+
+    protected virtual void ResetValue()
+    {
+        //For orverride
+    }
+}
